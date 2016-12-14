e16ec0fbfa6aed628b56241bfd9a420448f4636b
wangjin <wangjin-252@hotmail.com>
2016-12-14 23:13:46 +0800

    集成ant.Design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,20 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
+
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Button from 'antd/lib/button';
+
+import 'antd/dist/antd.css';
 
 let yeomanImage = require('../images/yeoman.png');
 
 export default class AppComponent extends React.Component {
+
+
+
   render() {
     return (
       <div className="index">
@@ -15,6 +22,8 @@ export default class AppComponent extends React.Component {
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
         <Header/>
         <Footer/>
+        <Button type="primary">Primary</Button>
+
       </div>
     );
   }
--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -3,11 +3,23 @@
  * Created by 王金 on 2016/12/9.
  */
 
-import React from 'react'
+require('styles/Menu.css');
+import React from 'react';
 
 export default class MenuItem extends React.Component {
 
   render() {
-    return (<div><a href={this.props.url}>{this.props.name}</a></div>)
+    return (<div>
+
+      <li>
+        <a
+          href={this.props.url}
+          title="Admin Dashboard"
+          className="sfActive">
+          <i className="glyph-icon icon-linecons-tv"/>
+          <span>{this.props.name}</span>
+        </a>
+      </li>
+    </div>)
   }
 }
